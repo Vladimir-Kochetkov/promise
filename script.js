6ce0c769b13299839c4a21f37b3a361b16b45e5b
Vladimir Kochetkov <js.kochetkov@gmail.com>
2022-03-11 06:35:42 +0300

    test

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+const getData = () => {
+    return fetch('db.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+};
+
+const user = getData();
+
+const sendData = (url, data) => {
+    return fetch(url, {
+        method: 'POST',
+        body: data,
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    });
+};
+
+sendData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(user))
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
